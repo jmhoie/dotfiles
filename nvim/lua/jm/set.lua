ceec16388e083f3e843f9f4bde6f6fdc9f171582
jmhoie <jmhoie@pm.me>
2025-09-17 12:35:56 +0200

    termguicolors

--- nvim/lua/jm/set.lua
+++ nvim/lua/jm/set.lua
@@ -1,27 +1,28 @@
-vim.o.guicursor = ""
-
+vim.o.mouse = "a"
+vim.o.showmode = false
+vim.o.breakindent = true
+vim.o.splitright = true
+vim.o.splitbelow = true
+vim.o.inccommand = "split"
+vim.o.cursorline = true
+vim.o.scrolloff = 10
+vim.o.confirm = true
+vim.o.ignorecase = true
+vim.o.smartcase = true
 vim.o.tabstop = 4
 vim.o.softtabstop = 4
 vim.o.shiftwidth = 4
 vim.o.expandtab = true
-
 vim.o.smartindent = true
-
 vim.o.wrap = false
-
 vim.o.swapfile = false
 vim.o.backup = false
 vim.o.undodir = os.getenv("HOME") .. "/.vim/undodir"
 vim.o.undofile = true
-
 vim.o.hlsearch = false
 vim.o.incsearch = true
-
+vim.o.updatetime = 50
+vim.o.colorcolumn = "80"
 vim.o.termguicolors = true
 
-vim.o.scrolloff = 10
-vim.o.signcolumn = "yes"
-
-vim.o.updatetime = 50
-
-vim.o.colorcolumn = "80"
+vim.cmd "colorscheme vim"
